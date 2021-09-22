2f11caa349bba9c80bc19d84a839ad138b8381f3
IngridFerreira93 <ingrid.b.f@hotmail.com>
2021-09-21 21:20:18 -0300

    pagina Header

--- pokedex1/src/components/Header/Header.js
+++ pokedex1/src/components/Header/Header.js
@@ -1,10 +1,38 @@
 import React from "react";
+import { useHistory } from 'react-router-dom'
+import { HeaderDiv, Title } from './Styled'
 
+const Header = (props) => {
+    const history = useHistory()
 
-function Header (){
-    return(
+    const goToPokedex = () => {
+        history.push('/pokedex')
+    }
+
+    const goToList = () => {
+        history.push('/')
+    }
+
+    return (
         <div>
-            <h1>Header</h1>
+            
+                <HeaderDiv>
+                    <button onClick={goToPokedex}>Ir Para Pokedex</button>
+                    <Title>Lista de Pokemons</Title>
+                    <p></p>
+                </HeaderDiv>
+            
+                <HeaderDiv>
+                    <button onClick={goToList}>Voltar para a Lista de Pokemons</button>
+                    <Title>Pokedex</Title>
+                    <p></p>
+                </HeaderDiv>
+            
+                <HeaderDiv>
+                    <button onClick={goToList}>Voltar</button>
+                   <Title>{props.name}</Title>
+                    <button onClick={goToPokedex}>Ir Para Pokedex</button>
+                </HeaderDiv>
         </div>
     )
 }
---
+++ pokedex1/src/components/Header/Styled.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+export const HeaderDiv = styled.div `
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding-right: 2vw;
+    padding-left: 1vw;
+    background-color: lightskyblue;
+    width:97vw;
+    position:absolute;
+    height: 8vh;
+`
+export const Title = styled.h1 `
+    color: white;
+`
